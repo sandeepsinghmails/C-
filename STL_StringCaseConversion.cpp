e6e3ae4fd0eccebd87fac0f5b1c2de176e97ac1d
Sandeep Singh <sandeepsinghmails@gmail.com>
2018-04-29 14:06:01 +0530

    Create STL_StringCaseConversion.cpp

---
+++ STL_StringCaseConversion.cpp
@@ -0,0 +1,25 @@
+// Author: Sandeep Singh
+// Date: April 29, 2018
+// Convert a std::string to uppercase or lowercase using STL
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // su is the string which is converted to uppercase
+    string str = "Sample String";
+
+    // using transform() function and ::toupper in STL
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    cout << str << endl;
+
+    // sl is the string which is converted to lowercase
+    string str2 = "Sample String";
+
+    // using transform() function and ::tolower in STL
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+    cout << str2 << endl;
+
+    return 0;
+}
